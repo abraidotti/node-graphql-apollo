d2c0f45322d2dd923d44e0ce8293bd3b7530ab2d
Alessandro Braidotti <alessandro@kintyre.co>
2020-08-25 11:46:44 -0400

    add first part of tutorial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+import 'cross-fetch/polyfill';
+import ApolloClient from 'apollo-boost';
+ 
 import 'dotenv/config';
-
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
-
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
-
-console.log(user);
-
-console.log(process.env.SOME_ENV_VARIABLE);
+ 
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
